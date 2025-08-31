3ba5aa88e3d9d8f0da019dad849a0a2e6c4c7836
Lyubov9302 <lyubovyurynets@gmail.com>
2025-08-31 16:15:31 +0300

    fixed handleVote function

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -10,10 +10,10 @@ export default function App() {
   const [votes, setVotes] = useState<Votes>({ good: 0, neutral: 0, bad: 0 });
 
   const handleVote = (type: VoteType) => {
-    setVotes({
-      ...votes,
-      [type]: votes[type] + 1,
-    });
+    setVotes((prev) => ({
+      ...prev,
+      [type]: prev[type] + 1,
+    }));
   };
   const resetVotes = () => {
     setVotes({ good: 0, neutral: 0, bad: 0 });
