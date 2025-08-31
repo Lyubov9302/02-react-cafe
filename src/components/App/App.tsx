9d9749a16173b525bf81cc7f788fec70cd335e67
Lyubov9302 <lyubovyurynets@gmail.com>
2025-08-31 11:54:24 +0300

    added useState

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,21 +1,31 @@
 import css from "./App.module.css";
 import CafeInfo from "../CafeInfo/CafeInfo";
-import Votes from "../../types/votes";
+import { Votes, VoteType } from "../../types/votes";
 import { useState } from "react";
 
 export default function App() {
-  const [votes, setVotes] = useState(0);
+  const [votes, setVotes] = useState<Votes>({ good: 0, neutral: 0, bad: 0 });
 
-  const handleVote = () => {
-    setVotes(votes + 1);
+  const handleVote = (type: VoteType) => {
+    setVotes({
+      ...votes,
+      [type]: votes[type] + 1,
+    });
   };
+  const resetVotes = () => {
+    setVotes({ good: 0, neutral: 0, bad: 0 });
+  };
+
   return (
     <>
       <div className={css.app}>
         <CafeInfo />
-        <button onClick={handleVote}>Good: {votes}</button>;
-        <button onClick={handleVote}>Neutral: {votes}</button>;
-        <button onClick={handleVote}>Bad: {votes}</button>;
+        <button onClick={() => handleVote("good")}>Good: {votes.good}</button>
+        <button onClick={() => handleVote("neutral")}>
+          Neutral: {votes.neutral}
+        </button>
+        <button onClick={() => handleVote("bad")}>Bad: {votes.bad}</button>
+        <button onClick={resetVotes}>Reset</button>
       </div>
     </>
   );
